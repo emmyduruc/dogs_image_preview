2c49cd0cffe6896b15f9ad0aeae3b5c198362ccd
emmyduruc <82957750+emmyduruc@users.noreply.github.com>
2021-11-12 19:10:47 +0100

    showing the list of 50 images

--- pages/components/home/HomePage.tsx
+++ pages/components/home/HomePage.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import {  Container, Card, } from "react-bootstrap";
+import ImagesHooks from "../hooks/imageHooks";
 
-function HomePage() {
-       return (
-              <div>
-                     
-              </div>
-       )
-}
+const HomePage = () => {
+  const [error, myImages] = ImagesHooks() as [unknown, string[]];
+  return (
+    <Container>
+      {myImages &&
+        myImages.map((image: string) => (
+          <Card key={image}>
+            <Card.Img variant="top" src={image} />
+            <Card.Title>Card Title</Card.Title>
+          </Card>
+        ))}
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ pages/components/hooks/imageHooks.ts
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type Content = {
+  message: string[];
+};
+
+export default function ImagesHooks() {
+  const [myImages, setMyImages] = useState<string[]>();
+  const [error, setError] = useState<unknown>();
+
+  useEffect(() => {
+    const fetchDogs = async () => {
+      try {
+        const result = await axios.get<Content>(
+          "https://dog.ceo/api/breeds/image/random/50"
+        );
+        const finalDogResult = result.data.message;
+        console.log("finalResult", finalDogResult, typeof finalDogResult);
+        setMyImages(finalDogResult);
+      } catch (error) {
+        console.log("error", error);
+        setError(error);
+      }
+    };
+    fetchDogs();
+  }, []);
+  return [error, myImages];
+}
