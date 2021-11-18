5c0175202d2a69798c4ba871bda31cc74d91f163
emmyduruc <82957750+emmyduruc@users.noreply.github.com>
2021-11-18 17:05:24 +0100

    modify image cout

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -1,39 +1,56 @@
 import React from "react";
 import axios from "axios";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { v4 as uuidv4 } from "uuid";
 import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import InfiniteScroll from "react-infinite-scroll-component";
 import ImageModal from "../modal/Modal";
 import Loader from "./Loader";
 import { Card, Badge } from "react-bootstrap";
-import { countAtom, imageAtom, modalAtom } from "../recoils/atoms";
+import { imageAtom, ImageObject, isCount, modalAtom } from "../recoils/atoms";
 import { Container } from "./style";
 
-const HomePage = () => {
-  const [count, setCount] = useRecoilState(countAtom);
+type Result = {
+    result: string[]
+}
+const HomePage = ({result}:Result) => {
+  console.log("result", result);
+
+  
+  const imageCount = useRecoilValue(isCount);
   const [modalShow, setModalShow] = useState<boolean>(false);
   const [img, setModalImg] = useRecoilState(modalAtom);
   const [images, setImages] = useRecoilState(imageAtom);
 
-  const handleClick = (image: string, index: number) => {
+  const handleClick = (image: ImageObject) => {
     setModalShow(true);
-    setModalImg(image);
-    const newArray = [...count];
-    newArray[index] += 1;
-    setCount(newArray);
+    setModalImg(image.url);
+    const updatedCount = imageCount.map((imageItem) => {
+      if (image.id === imageItem.id) {
+        return { ...image, count: image.count + 1 };
+      } else {
+        return imageItem;
+      }
+    });
+    setImages(updatedCount);
   };
   const fetchImages = async (num = 25) => {
     await axios
-      .get<any>(
+      .get(
         `https://dog.ceo/api/breeds/image/random/${
           images.length === 0 ? 50 : num
         }`
       )
       .then((res) => {
-        setImages([...images, ...res.data.message]);
-        const arr = [...count, ...new Array(res.data.message.length).fill(0)];
-        setCount(arr);
+        const mappedArray = res.data.message.map((image: ImageObject) => {
+          return {
+            id: uuidv4(),
+            url: image,
+            count: 0,
+          };
+        });
+        setImages([...images, ...mappedArray]);
       });
   };
   useEffect(() => {
@@ -49,24 +66,24 @@ const HomePage = () => {
         loader={<Loader />}
       >
         {images &&
-          images.map((image: string, index: number) => (
+          images.map((image: ImageObject) => (
             <Card
               aria-labelledby="dogs image card"
               style={{ margin: "10px", border: "none" }}
               onClick={() => {
-                handleClick(image, index);
+                handleClick(image);
               }}
               key={uuidv4()}
             >
               <Card.Img
                 aria-labelledby="dogs images"
                 variant="top"
-                src={image}
+                src={image.url}
                 alt="dogs images"
               />
               {image && (
                 <Card.Title aria-labelledby="preview counts">
-                  Previews: <Badge bg="secondary">{count[index]}</Badge>
+                  Previews: <Badge bg="secondary">{image.count}</Badge>
                 </Card.Title>
               )}
             </Card>
@@ -79,6 +96,21 @@ const HomePage = () => {
       </InfiniteScroll>
     </Container>
   );
+};
+
+type Post = {
+  message: string[]
+}
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://dog.ceo/api/breeds/image/random/50');
+  const posts = await res.json();
+  const result: Post[] = posts.data.message
+  // console.log("result", result);
+  return {
+    props: {
+      result,
+    },
+  };
 };
 
 export default HomePage;
--- components/recoils/atoms.ts
+++ components/recoils/atoms.ts
@@ -1,14 +1,26 @@
 import { atom, selector } from "recoil";
 
-export const countAtom = atom<number[]>({
-  key: "count-preview",
-  default: new Array(50).fill(0),
-});
-export const imageAtom = atom<string[]>({
+export type ImageObject = {
+id: string,
+url: string,
+count: number
+}
+// export const countAtom = atom<number[]>({
+//   key: "count-preview",
+//   default: new Array(50).fill(0),
+// });
+export const imageAtom = atom<ImageObject[]>({
   key: "image-atoms",
   default: [],
 });
 export const modalAtom = atom({
   key: "modal-atom",
   default: "",
+});
+export const isCount = selector({
+  key: "lengthState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const image = get(imageAtom);
+    return image;
+  },
 });
